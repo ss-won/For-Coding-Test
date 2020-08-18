a96b8378a7d82f587c2f4edc6ea3cc21cc7321ae
Sowon Jung <ss_won_@naver.com>
2020-08-18 17:01:29 +0900

    improving: link4.js
    Edit: link4.js, linkedlist.js
    
    * I remove prototype function named remove in link4.js and reimplemented it in linkedlist.js
    
    * Existing function in link4.js has not checked case of removing head node. so, I improved it while reimplementing.

--- Basic/Javascript/Datastructure/link4.js
+++ Basic/Javascript/Datastructure/link4.js
@@ -1,23 +1,14 @@
 // # 중복된 원소를 제거한다.
 import { Linkedlist } from './linkedlist.js';
 
-Linkedlist.prototype.remove = function(node) {
-    let cur = this.head;
-    while (cur.next !== node) {
-        cur = cur.next;
-    }
-    cur._next = cur.next.next;
-    //cur._next._next = null;
-}
-
-Linkedlist.prototype.deduplicate = function () {
-    let cur = this.head;
+function deduplicate(obj) {
+    let cur = obj.head;
     let map = new Map();
     while(cur){
         // 중복된 데이터를 찾을 때마다 해당 노드 삭제
         // Whenever an overlap value is found, the node is deleted.
         if(map.has(cur.data)){
-            this.remove(cur);
+            obj.remove(cur);
         }
         else{
             map.set(cur.data, 1);
@@ -26,7 +17,7 @@ Linkedlist.prototype.deduplicate = function () {
     }
 }
 
-
+/*
 // @ Test
 let ll = new Linkedlist();
 for(var i=1;i<6;i++){
@@ -36,6 +27,7 @@ for(i;i>2;i--){
     ll.add(i);
 }
 ll.print();//1,2,3,4,5,6,5,4,3
-ll.deduplicate();
+deduplicate(ll);
 console.log("\n")
 ll.print();//1,2,3,4,5,6
+*/
--- Basic/Javascript/Datastructure/linkedlist.js
+++ Basic/Javascript/Datastructure/linkedlist.js
@@ -2,7 +2,7 @@
 //** some methods aren 't implemented that because they will be revised in other problem **
 
 class Node {
-    constructor(data, next=null){
+    constructor(data, next = null) {
         this._data = data;
         this._next = next;
     }
@@ -21,9 +21,9 @@ class Node {
 }
 
 class Linkedlist {
-     //head
-    constructor(data=null){
-        this._head = (data!=null) ? new Node(data) : data;
+    //head
+    constructor(data = null) {
+        this._head = (data != null) ? new Node(data) : data;
     }
 
     get head() {
@@ -32,34 +32,73 @@ class Linkedlist {
 
     //Method - isEmpty(role : checking linkedlist empty)
     isEmpty() {
-        return (this._head===null) ? true : false;
+        return (this._head === null) ? true : false;
     }
-    
+
     //Method - add(role : append node to linkedlist)
     add(data) {
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
             this._head = new Node(data);
-        }
-        else{
+        } else {
             let curr = this._head;
             //head가 있다면 탐색 후 맨 끝에 추가
-            while(curr.next){
+            while (curr.next) {
                 curr = curr.next;
             }
             curr.setNext = new Node(data);
         }
     }
 
-    //Method - print(role: print values ​​as they are in the linked dream)
+    //Method - print(role: print values ​​as they are in the linkedlist)
     print() {
         let curr = this._head;
-        while(curr){
+        while (curr) {
             console.log(curr.data);
             curr = curr.next;
         }
     }
 
- }
+    //Method - print(role: remove (nth or any) node in the linkedlist)
+    remove(node) {
+        if (typeof node === 'object') {
+            let cur = this.head;
+            if (cur === node) {
+                //head일때
+                this._head = cur.next;
+                cur._next = null;
+            }
+            while (cur.next && cur.next !== node) {
+                console.log(cur);
+                cur = cur.next;
+            }
+            if (cur.next)
+                cur._next = cur.next.next;
+        } else if (typeof node === 'number') {
+            //head : 0번째
+            let i = 0;
+            let cur = this.head;
+            if (node === i) {
+                //head일때,
+                this._head = cur.next;
+                cur._next = null;
+            }
+            while (i !== node - 1) {
+                if (!cur.next) {
+                    return false;
+                }
+                cur = cur.next;
+                i++;
+            }
+            cur._next = cur.next.next;
+        }
+    }
+
+}
+
+export {
+    Node,
+    Linkedlist
+};
 
 /* ============testing============*/
 //let node = new Node(3);
@@ -70,7 +109,15 @@ class Linkedlist {
 //for(let i=0; i<10;i++){
 //    linkedlist.add(i);
 //}
+//
 //linkedlist.print();
+//linkedlist.remove(3);
+//console.log("\n")
+//linkedlist.print();
+//console.log(linkedlist.head);
+//
+//linkedlist.remove(0);
+//console.log("\n")
+//linkedlist.print();
+//console.log(linkedlist.head);
 /*==============================*/
-
-export { Node, Linkedlist };
