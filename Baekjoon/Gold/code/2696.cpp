55b4b55b3a475092c809518336b49d1c60340976
Wish Jung <ss_won_@naver.com>
2020-10-09 22:50:12 +0900

    update: baekjoon 2696.cpp
    - add extra condition

--- Baekjoon/Gold/code/2696.cpp
+++ Baekjoon/Gold/code/2696.cpp
@@ -4,56 +4,48 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int t, m, input;
+int t, m, input, c = 0;
 priority_queue<int, vector<int>, less<int>> mxq;
 priority_queue<int, vector<int>, greater<int>> mnq;
 
-int main()
-{
-    ios_base ::sync_with_stdio(false);
+int main() {
+    ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cin >> t;
-    while (t--)
-    {
+    while(t--){
         cin >> m;
         priority_queue<int, vector<int>, less<int>> mxq;
         priority_queue<int, vector<int>, greater<int>> mnq;
-        cout << (m / 2) + 1 << "\n";
-        for (int i = 1; i <= m; i++)
-        {
+        c = 0;
+        cout << (m/2)+1 << "\n";
+        for(int i=1;i<=m;i++){
             cin >> input;
-            if (mxq.empty() && mnq.empty())
-            {
+            if(mxq.empty() && mnq.empty()){
                 mxq.push(input);
             }
-            else if (mxq.size() > mnq.size())
-            {
-                if (mxq.top() > input)
-                {
+            else if(mxq.size() > mnq.size()){
+                if(mxq.top() > input){
                     mxq.push(input);
-                    mnq.push(mxq.top());
-                    mxq.pop();
+                    mnq.push(mxq.top()); mxq.pop();
                 }
-                else
-                {
+                else{
                     mnq.push(input);
                 }
             }
-            else if (mxq.size() == mnq.size())
-            {
-                if (mxq.top() < input)
-                {
+            else if(mxq.size() == mnq.size()){
+                if(mxq.top() < input){
                     mnq.push(input);
-                    mxq.push(mnq.top());
-                    mnq.pop();
+                    mxq.push(mnq.top()); mnq.pop();
                 }
-                else
-                {
+                else{
                     mxq.push(input);
                 }
             }
-            if (i % 2 == 1)
+            if(i%2==1){
                 cout << mxq.top() << " ";
+                c++;
+                if(c % 10 == 0) cout << "\n";
+            }
         }
         cout << "\n";
     }
